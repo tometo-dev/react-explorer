d32d317c32fd10f9ef559d776a5df4daa7cff051
Sudhanshu <perfectsudh@gmail.com>
2024-02-12 09:19:04 +0530

    add expand/collapse icons

--- src/components/treeview.tsx
+++ src/components/treeview.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 
 import { FolderType } from "~/types";
+import { cn } from "~/utils";
 
 export type TreeViewProps = {
   currentRoot: FolderType;
 };
+
+const ExpandIcon = ({ className, ...rest }: React.ComponentProps<"div">) => (
+  <div {...rest} className={cn(className, "min-w-0 max-w-min rotate-90 pt-1 text-center")}>
+    {">"}
+  </div>
+);
+
+const CollapseIcon = ({ className, ...rest }: React.ComponentProps<"div">) => (
+  <div {...rest} className={cn(className, "grid min-w-0 max-w-min -rotate-90 pt-1 text-center")}>
+    {">"}
+  </div>
+);
 
 export function TreeView({ currentRoot }: TreeViewProps) {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -13,13 +26,15 @@ export function TreeView({ currentRoot }: TreeViewProps) {
     <div className="flex flex-col gap-2">
       <div className="flex gap-1 px-2">
         <span>{currentRoot.name}</span>
-        <button
-          onClick={() => {
-            setIsExpanded(!isExpanded);
-          }}
-        >
-          {isExpanded ? "collapse" : "expand"}
-        </button>
+        <div id="button-panel" className="flex gap-1">
+          <button
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            {isExpanded ? <CollapseIcon /> : <ExpandIcon />}
+          </button>
+        </div>
       </div>
       {isExpanded && (
         <div className="pl-4">
